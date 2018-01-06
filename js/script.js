f3cf6af608e7c09c79ae87469eb6709699c98fc2
dodi-dodi <dorota.durlej@gmail.com>
2018-01-06 12:58:15 +0100

    button component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+class Btn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {clicked: null};
+
+        this.handleChoice = this.handleChoice.bind(this);
+    }
+    renderCircle() {
+        return (
+            <div className="icon-auto flex flex-center">
+                <i className="fa fa-circle-o fa-circle-color fa-large"/>
+            </div>
+        )
+    }
+    renderCross() {
+        return (
+            <div className="icon-auto flex flex-center">
+                <i className="fa fa-times fa-times-color fa-large"/>
+            </div>
+        )
+    }
+    renderDefault() {
+        return <button className="button-auto button-background" onClick={this.handleChoice}/>
+    }
+    handleChoice() {
+        this.setState({clicked: this.props.player})
+    }
+    render() {
+       if (this.state.clicked ===1) {
+           return this.renderCross();
+       } else if (this.state.clicked ===2) {
+           return this.renderCircle();
+       } else {
+           return this.renderDefault();
+       }
+    }
+}
+
+
 const element = (
     <div>
         <div className="flex flex-space-between">
@@ -13,39 +52,35 @@ const element = (
         <div>
             <div className="flex flex-center">
                 <div className="square">
-                    <div className="icon-auto flex flex-center">
-                        <i className="fa fa-times fa-times-color  fa-large"/>
-                    </div>
+                    <Btn player={1}/>
                 </div>
                 <div className="square border-vertical">
-                    <button className="button-auto button-background"/>
+                    <Btn player={1}/>
                 </div>
                 <div className="square">
-                    <div className="icon-auto flex flex-center">
-                        <i className="fa fa-circle-o fa-circle-color fa-large"/>
-                    </div>
+                    <Btn player={1}/>
                 </div>
             </div>
             <div className="flex flex-center">
                 <div className="square border-horizontal">
-                    <button className="button-auto  button-background"/>
+                    <Btn player={1}/>
                 </div>
                 <div className="square border-horizontal border-vertical">
-                    <button className="button-auto  button-background"/>
+                    <Btn player={1}/>
                 </div>
                 <div className="square border-horizontal">
-                    <button className="button-auto  button-background"/>
+                    <Btn player={1}/>
                 </div>
             </div>
             <div className="flex flex-center">
                 <div className="square">
-                    <button className="button-auto  button-background"/>
+                    <Btn player={1}/>
                 </div>
                 <div className="square border-vertical">
-                    <button className="button-auto  button-background"/>
+                    <Btn player={1}/>
                 </div>
                 <div className="square">
-                    <button className="button-auto  button-background"/>
+                    <Btn player={1}/>
                 </div>
             </div>
         </div>
